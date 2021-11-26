995da8e0d2a40e9ae4b4ca7b4ab83277be336e2d
andrea-s-hoffman <andrea.s.hoffman09@gmail.com>
2021-11-26 15:20:03 -0500

    initial commit

---
+++ 01-exercise-one/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// console.log(THREE);
+
+// need 4 elements
+// 1. scene
+// 2. object
+// 3. camera
+// 4. renderer
+
+const scene = new THREE.Scene();
+// console.log(scene);
+
+// red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// the above color is a three.js color class (0x replaces #)
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// camera
+const sizes = {
+  width: 800,
+  height: 600,
+};
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 2;
+camera.position.y = 1;
+scene.add(camera);
+
+// renderer
+const canvas = document.querySelector("canvas.webgl");
+console.log(canvas);
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
